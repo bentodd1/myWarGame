e389d46bcffad5ae7b859bc2bfe910bf1f2c8dbf
Ben Todd <btodd@multiservice.com>
2018-10-23 17:24:48 -0500

    Changed the stuff

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,22 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-left">
     <q-card>
       <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-      <div>My Value</div>
-      <div>Deck size</div>
-    </q-card>
-    <q-btn
+       <q-btn
     @click="startGame" >
       Start game
       </q-btn>
-      <q-btn
+       <div>Cards in deck {{this.player1.deck.length}} Cards in discard {{this.player1.discardDeck.length}}</div>
+       <div>{{player1.currentCard}}</div>
+    </q-card>
+      <q-card>
+      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+          <q-btn
       @click="flipCards([])">
        Flip flipCards
       </q-btn>
-      <q-card>
-      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-      <div>deck size</div>
+      <div>Cards in deck {{this.player2.deck.length}} Cards in discard {{this.player2.discardDeck.length}}</div>
+      <div>{{player2.currentCard}}</div>
     </q-card>
   </q-page>
 </template>
@@ -24,36 +25,40 @@
 </style>
 
 <script>
-import deck from '../assets/deck'
+import starterDeck from '../assets/deck'
 export default {
-
   name: 'PageIndex',
   data () {
     return {
       player1: {
         deck: [],
-        discardDeck: []
+        discardDeck: [],
+        currentCard: 0
       },
       player2: {
         deck: [],
-        discardDeck: []
-      }
+        discardDeck: [],
+        currentCard: 0
+      },
+      mainDeck: []
     }
   },
   methods: {
+
     async startGame () {
-      await this.shuffle(deck)
+      this.mainDeck = starterDeck
+      await this.shuffle(this.mainDeck)
       console.log('Game has started')
-      console.log(deck)
+      console.log(this.mainDeck)
       await this.dealToEachPlayer()
     },
 
     dealToEachPlayer () {
-      let splitDeck = deck.splice(26)
+      let splitDeck = this.mainDeck.splice(26)
       this.player1.deck = splitDeck
-      this.player2.deck = deck
+      this.player2.deck = this.mainDeck
       console.log(splitDeck)
-      console.log(deck)
+      console.log(this.mainDeck)
     },
     shuffle (array) {
       for (var i = array.length - 1; i > 0; i--) {
@@ -94,11 +99,13 @@ export default {
       console.log(this.player1.discardDeck)
       console.log(this.player2.discardDeck)
       let player1Card = await this.player1.deck.shift()
+      this.player1.currentCard = player1Card
       let player2Card = await this.player2.deck.shift()
+      this.player2.currentCard = player2Card
       let player1CardValue = player1Card.charAt(0)
       let player2CardValue = player2Card.charAt(0)
-      discardPile.push(player1Card)
-      discardPile.push(player2Card)
+      discardPile.push(this.player1Card)
+      discardPile.push(this.player2Card)
       if (player1CardValue > player2CardValue) {
         console.log('player 1 card is larger')
         this.takeDiscardPile(this.player1, discardPile)
